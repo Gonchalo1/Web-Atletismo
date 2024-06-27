2f64477f521d535eeaaf007a7d15bc2266a9bdf8
G <gonzalocal127@gmail.com>
2024-06-27 17:12:17 -0300

    Progresando en el carousel

---
+++ src/home/carousel2/carousel2.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import './carousel2.css';
+import Miembro1 from '../../imagenes/10.jpeg'; 
+import Miembro2 from '../../imagenes/11.jpeg';
+import Miembro3 from '../../imagenes/12.jpeg';
+import Miembro4 from '../../imagenes/13.jpeg'; 
+import Miembro5 from '../../imagenes/14.jpeg';
+import Miembro6 from '../../imagenes/15.jpeg';
+
+function Carousel2() {
+  return (
+    <div className='div-carousel2'>
+      <h3 className='carousel2-h3'><strong>Miembros</strong></h3>
+      <Carousel>
+        <Carousel.Item>
+          <div className="carousel-content">
+            <a href="https://instagram.com/brian_cardozo11" target="_blank" rel="noopener noreferrer" className="link-contenido">
+              <div className="contenido2">
+                <img src={Miembro1} alt="Brian Cardozo" className="miembro-img" />
+                <div className="texto">
+                  <p className="nombre">Brian Cardozo</p>
+                  <p className="instagram">@brian_cardozo11</p>
+                </div>
+              </div>
+            </a>
+            <a href="https://instagram.com/otro_miembro" target="_blank" rel="noopener noreferrer" className="link-contenido">
+              <div className="contenido2">
+                <img src={Miembro2} alt="Otro Miembro" className="miembro-img" />
+                <div className="texto">
+                  <p className="nombre">Otro Miembro</p>
+                  <p className="instagram">@otro_miembro</p>
+                </div>
+              </div>
+            </a>
+            <a href="https://instagram.com/tercer_miembro" target="_blank" rel="noopener noreferrer" className="link-contenido">
+              <div className="contenido2">
+                <img src={Miembro3} alt="Tercer Miembro" className="miembro-img" />
+                <div className="texto">
+                  <p className="nombre">Tercer Miembro</p>
+                  <p className="instagram">@tercer_miembro</p>
+                </div>
+              </div>
+            </a>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="carousel-content">
+            <a href="https://instagram.com/cuarto_miembro" target="_blank" rel="noopener noreferrer" className="link-contenido">
+              <div className="contenido2">
+                <img src={Miembro4} alt="Cuarto Miembro" className="miembro-img" />
+                <div className="texto">
+                  <p className="nombre">Cuarto Miembro</p>
+                  <p className="instagram">@cuarto_miembro</p>
+                </div>
+              </div>
+            </a>
+            <a href="https://instagram.com/quinto_miembro" target="_blank" rel="noopener noreferrer" className="link-contenido">
+              <div className="contenido2">
+                <img src={Miembro5} alt="Quinto Miembro" className="miembro-img" />
+                <div className="texto">
+                  <p className="nombre">Quinto Miembro</p>
+                  <p className="instagram">@quinto_miembro</p>
+                </div>
+              </div>
+            </a>
+            <a href="https://instagram.com/sexto_miembro" target="_blank" rel="noopener noreferrer" className="link-contenido">
+              <div className="contenido2">
+                <img src={Miembro6} alt="Sexto Miembro" className="miembro-img" />
+                <div className="texto">
+                  <p className="nombre">Sexto Miembro</p>
+                  <p className="instagram">@sexto_miembro</p>
+                </div>
+              </div>
+            </a>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+export default Carousel2;
--- src/home/home.js
+++ src/home/home.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Carousel from './carousel/carousel';
 import Footer from '../footer/footer';
+import Carousel2 from './carousel2/carousel2';
+import PrecioFondo from '../imagenes/homeImagenes/Precio.png';
+import './home.css';
 
 function Home() {
-    return (
-      <div className="home-principal">
-        <Carousel/>
+  return (
+    <div className="home-principal">
+      <Carousel />
+      <Link to="/precios" className="home2-link">
         <div className='home2'>
           <div className='home2-h3'>
-            <h3>
-              Inscripciones Online
-            </h3>
+            <h3>Inscripciones Online</h3>
           </div>
           <div className='home2-imagen'>
-            <img></img>
+            <img src={PrecioFondo} alt="Inscripciones Online" />
           </div>
         </div>
-        <Footer/>
-      </div>
-    );}
-  
+      </Link>
+      <Carousel2 />
+      <Footer />
+    </div>
+  );
+}
+
 export default Home;
