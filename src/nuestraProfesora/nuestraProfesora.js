b7122c8650b736de2e768810e826680c40cc2f8b
G <gonzalocal127@gmail.com>
2024-07-04 14:36:22 -0300

    Trabajando en la ultima seccion

--- src/nuestraProfesora/nuestraProfesora.js
+++ src/nuestraProfesora/nuestraProfesora.js
@@ -6,7 +6,6 @@ import Carousel1 from '../imagenes/carousel1.jpeg';
 import Carousel2 from '../imagenes/carousel2.jpeg';
 import Carousel3 from '../imagenes/carousel3.jpeg';
 
-
 function NuestraProfesora() {
   return (
     <div className="principalPame" style={{ backgroundImage: `url(${fondo})` }}>
@@ -18,21 +17,21 @@ function NuestraProfesora() {
         </div>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src={Carousel1} className="d-block w-100" alt="Primer Slide"/>
+            <img src={Carousel1} className="d-block w-100 mx-auto" alt="Primer Slide"/>
             <div className="carousel-caption d-none d-md-block">
               <h5>Primer Título</h5>
               <p>Descripción para la primera imagen.</p>
             </div>
           </div>
           <div className="carousel-item">
-            <img src={Carousel2} className="d-block w-100" alt="Segundo Slide"/>
+            <img src={Carousel2} className="d-block w-100 mx-auto" alt="Segundo Slide"/>
             <div className="carousel-caption d-none d-md-block">
               <h5>Segundo Título</h5>
               <p>Descripción para la segunda imagen.</p>
             </div>
           </div>
           <div className="carousel-item">
-            <img src={Carousel3} className="d-block w-100" alt="Tercer Slide"/>
+            <img src={Carousel3} className="d-block w-100 mx-auto" alt="Tercer Slide"/>
             <div className="carousel-caption d-none d-md-block">
               <h5>Tercer Título</h5>
               <p>Descripción para la tercera imagen.</p>
