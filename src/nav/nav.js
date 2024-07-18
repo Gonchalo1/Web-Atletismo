b68b665b577aa0a9b2ae33d497e5000bf67ccaf0
G <gonzalocal127@gmail.com>
2024-07-18 10:24:04 -0300

    Cambios de mantenimiento

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../imagenes/logo.png';
 import '../App.css';
 
 function Nav() {
+    const emailRef = useRef(null);
+
+    const copyToClipboard = (text) => {
+        const el = document.createElement('textarea');
+        el.value = text;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        alert("Email copiado al portapapeles");
+    };
+
+    const handleCopyEmail = () => {
+        copyToClipboard('PamelaMaidana@gmail.com');
+    };
+
     return (
         <div>
             <div className="header-container">
@@ -24,7 +40,9 @@ function Nav() {
                     <h4>
                         <div>
                             <p className="mb-0 email-text">Email:</p>
-                            <p className="mb-0 email-text">PamelaMaidana@gmail.com</p>
+                            <p className="mb-0 email-text copyable-text" onClick={handleCopyEmail}>
+                                PamelaMaidana@gmail.com
+                            </p>
                         </div>
                     </h4>
                 </div>
@@ -34,7 +52,7 @@ function Nav() {
                 <nav className="navbar navbar-expand-lg navbar-light">
                     <div className="container-fluid justify-content-center">
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <i className="bi bi-list navbar-toggler-icon-custom"></i>
+                            <i className="bi bi-list navbar-toggler-icon-custom"></i>
                         </button>
                         <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
                             <ul className="navbar-nav mb-2 mb-lg-0">
@@ -55,15 +73,15 @@ function Nav() {
                                 </li>
                             </ul>
                             <div className="header-actions ml-lg-3">
-                            <a
-                            href="https://www.instagram.com/rprunningteam/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            >
-                        <i className="bi bi-instagram" ></i>
-                            </a>
-                            <Link to="nuestraProfesora">
-                                <button className="btn btn-light header-button d-none d-lg-inline-block text-dark">Nuestra Profesora</button>
+                                <a
+                                    href="https://www.instagram.com/rprunningteam/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <i className="bi bi-instagram"></i>
+                                </a>
+                                <Link to="nuestraProfesora">
+                                    <button className="btn btn-light header-button d-none d-lg-inline-block text-dark">Nuestra Profesora</button>
                                 </Link>
                             </div>
                         </div>
